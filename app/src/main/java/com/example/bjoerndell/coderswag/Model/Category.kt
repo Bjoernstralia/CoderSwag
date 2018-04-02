25bb47f04dd2913396491653c48f205d5b715d95
Bjoernstralia <B.Viehmann@gmx.de>
2018-04-02 21:21:13 +0200

    Added classes

---
+++ app/src/main/java/com/example/bjoerndell/coderswag/Model/Category.kt
@@ -0,0 +1,6 @@
+package com.example.bjoerndell.coderswag.Model
+
+/**
+ * Created by BjoernDell on 02.04.2018.
+ */
+class Category (val title: String, val image: String)
---
+++ app/src/main/java/com/example/bjoerndell/coderswag/Model/Product.kt
@@ -0,0 +1,6 @@
+package com.example.bjoerndell.coderswag.Model
+
+/**
+ * Created by BjoernDell on 02.04.2018.
+ */
+class Product (val title: String, val price:  String, val image: String)

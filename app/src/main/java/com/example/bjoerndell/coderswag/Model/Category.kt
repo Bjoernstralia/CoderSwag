6567235f9a6bd7ba94d383aea14522fb81bb0d04
Bjoernstralia <B.Viehmann@gmx.de>
2018-04-08 11:05:58 +0200

    new drawables

--- app/src/main/java/com/example/bjoerndell/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/bjoerndell/coderswag/Model/Category.kt
@@ -3,4 +3,9 @@ package com.example.bjoerndell.coderswag.Model
 /**
  * Created by BjoernDell on 02.04.2018.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+
+}
--- app/src/main/java/com/example/bjoerndell/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/bjoerndell/coderswag/Services/DataService.kt
@@ -1,7 +1,41 @@
 package com.example.bjoerndell.coderswag.Services
 
+import com.example.bjoerndell.coderswag.Model.Category
+import com.example.bjoerndell.coderswag.Model.Product
+
 /**
  * Created by BjoernDell on 02.04.2018.
  */
 object DataService {
+
+    val categories = listOf(
+            Category ("SHIRTS", "shirtimage"),
+            Category ("HATS", "hatimage"),
+            Category("HODDIES", "hoodieimage"),
+            Category("DIGITAL", "digitalgoodsimage")
+    )
+
+    val shirts = listOf(
+            Product("Devslopes Shirt Black", "$18", "shirt01"),
+            Product("Devslopes Badge Light Gray", "$20", "shirt02"),
+            Product("Devslopes Logo Shirt Red", "$22", "shirt03"),
+            Product("Devslopes Hustle", "$22", "shirt04"),
+            Product("Kickflip Studios", "18", "shirt05")
+    )
+
+    val hats = listOf(
+            Product("Devslopes Graphic Beanie", "$18", "hat01"),
+            Product("Devslopes Hat Black", "$20", "hat02"),
+            Product("Devslopes Hat White", "$18", "hat03"),
+            Product("Devslopes Hat Snapback", "$22", "hat04")
+    )
+
+    val hoodies = listOf(
+            Product("Devslopes Hoodie Gray", "$28", "hoodie01"),
+            Product("Devslopes Hoodie Red", "$32", "hoodie02"),
+            Product("Devslopes Gray Hoodie", "$28", "hoodie03"),
+            Product("Devslopes Black Hoodie", "$32", "hoodie04")
+    )
+
+
 }
